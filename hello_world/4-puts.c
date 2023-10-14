87f815e82b1ef0a39931e75fcfa97b8b719dabc9
aelitaa <gunel.sultanova@outlook.com>
2023-10-14 12:57:15 -0700

    maria

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(void) /* one-liner comment on line 7 */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
