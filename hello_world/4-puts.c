d4fdb3a7faef8f8491c5c66418541b65796b9be5
aelitaa <gunel.sultanova@outlook.com>
2023-10-14 13:52:14 -0700

    help

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,19 +1,15 @@
-/*
+/**
  * File: 4-puts.c
  */
 
 #include <stdio.h>
 
-/*
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+/**
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
-int main(void) /* 
-		* main - Prints "Programming is like building
-		* a multilingual puzzle, followed by a new line.
-*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
