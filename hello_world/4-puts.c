46a23d8b343d8fb461720c25f2252be8912172c4
aelitaa <gunel.sultanova@outlook.com>
2023-10-14 13:26:57 -0700

    what

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -10,8 +10,7 @@
  *
  * Return: Always 0.
  */
-int main(void) /*main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.*/
+int main(void) /* main - Prints "Programming is like building a multilingual puzzle, followed by a new line.*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
