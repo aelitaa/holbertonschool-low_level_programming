6324cc00cbd11cc388f04ea94f1b079173388f2f
aelitaa <gunel.sultanova@outlook.com>
2023-10-14 13:11:28 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
-int main(void)
+int main(void)/* main - prints "Programming is like building a multilingual puzzle, followed by a newline*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
