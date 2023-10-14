35e7d51b5bdbcac3fb21d341ea2d0ea7294fb9b3
aelitaa <gunel.sultanova@outlook.com>
2023-10-14 13:56:02 -0700

    dev

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,9 +5,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
