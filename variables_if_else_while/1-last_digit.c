7428d8f6807489c9a7309dbbb4ef0c8b517a5b72
aelitaa <gunel.sultanova@outlook.com>
2023-10-18 03:38:42 -0700

    betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-        int n;
+  int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
@@ -23,6 +23,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n",
 			n, n % 10);
-	}	
+	}
 	return (0);
 }
