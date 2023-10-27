e352d5fff532e7ad3fbd7669b91bbcd4ee1238fa
aelitaa <gunel.sultanova@outlook.com>
2023-10-27 04:17:13 -0700

    string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - outputs a string in reverse.
+ *
+ * @s: The string to print.
+ */
+void rev_string(char *s)
+{
+int length = strlen(s);
+int i;
+
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+
+_putchar('\n');
+}
