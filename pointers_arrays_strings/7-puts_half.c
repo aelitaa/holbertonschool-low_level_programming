7cfce173656ba2c17420bdab4f055cef46b4a9dd
aelitaa <gunel.sultanova@outlook.com>
2023-10-28 01:14:11 -0700

    betty

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,10 +9,10 @@ void puts_half(char *str)
 {
 int length = strlen(str);
 int i;
-int start = length / 2 ;
-if ( length % 2 != 0 )
+int start = length / 2;
+if (length % 2 != 0)
 {
-     start++;
+start++;
 }
 for (i = start ; i < length; i++)
 {
