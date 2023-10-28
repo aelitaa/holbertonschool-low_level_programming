742253562040faf1b481ad01aea58085558cc331
aelitaa <gunel.sultanova@outlook.com>
2023-10-28 00:37:23 -0700

    second

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - outputs second half of string
+ *
+ * @str: The string to print.
+ */
+void puts_half(char *str)
+{
+int length = strlen(str);
+int i;
+
+for (i = length / 2 ; i < length; i++)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
