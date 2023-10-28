03d0ed3abf5fc8a52ca9675b172cce732fd579c3
aelitaa <gunel.sultanova@outlook.com>
2023-10-27 22:28:08 -0700

    every other

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - outputs only every other character of string
+ *
+ * @str: The string to print.
+ */
+void void puts2(char *str)
+{
+int length = strlen(str);
+int i;
+
+for (i = 0; i < length; i+=2)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
