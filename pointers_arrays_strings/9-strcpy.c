0126da352b341b77fa619d3283de220373ec5b66
aelitaa <gunel.sultanova@outlook.com>
2023-10-28 03:04:10 -0700

    trouble

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * print_array - copies from src to dest
+ * _strcpy - copies from src to dest
  *
  * @dest: Copied to.
  * @src: Copied from.
+ * @dest_start: returned value.
  */
-char *_strcpy(char *dest, char *src) {
-    char *dest_start = dest; 
-    while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return dest_start;
+char *_strcpy(char *dest, char *src)
+{
+char *dest_start = dest;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return dest_start;
 }
