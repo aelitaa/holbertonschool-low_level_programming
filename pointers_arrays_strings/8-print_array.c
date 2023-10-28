e8c81374e6b6ec23f1204fdb3a8826e30fdb0a8a
aelitaa <gunel.sultanova@outlook.com>
2023-10-28 02:37:24 -0700

    fixed

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,16 +9,16 @@
  */
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0 ; i < n ; i++)
-{
-  if(i < n - 1)
-    {
-      printf("%d, ", a[i]);
-    }else
-    {
-      printf("%d\n", a[i]);
-    }
-}
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[index]);
+
+		if (i == n - 1)
+			continue;
+
+		printf(", ");
+	}
+	printf("\n");
 }
