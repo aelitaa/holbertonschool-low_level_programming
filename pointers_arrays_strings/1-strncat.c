8f32abd6238ef61bcec765bbe70108eb3dd7243d
aelitaa <gunel.sultanova@outlook.com>
2023-10-29 03:38:30 -0700

    hua

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,18 +10,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int index;
-char *dest_original = dest;
+	int index = 0, dest_len = 0;
 
-while (*dest != '\0')
-dest++;
-for (index = 0; index < n; index++)
-{
-*dest = *src;
-dest++;
-src++;
-}
-*dest = '\0';
+	while (dest[index++])
+		dest_len++;
 
-return (dest_original);
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
