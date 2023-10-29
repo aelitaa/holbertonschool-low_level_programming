fc30de03f350c06e1a3dfdd8b444810651eb277b
aelitaa <gunel.sultanova@outlook.com>
2023-10-29 03:12:26 -0700

    special

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
+ * _strcat - Concatenates the string pointed to by @src, including the terminating
  *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 char *dest_original = dest;
 
