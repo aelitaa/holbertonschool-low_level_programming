e4a424bea2d9cc11b95652dd27549ecfcff48e4f
aelitaa <gunel.sultanova@outlook.com>
2023-10-29 04:14:27 -0700

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * memset - Fills the first n byte.
+ * _memset - Fills the first n byte.
  *
  * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
+ * @b: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
  * Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
-
+	
 	for (index = 0; index < n; index++)
-		memory[index] = value;
+		s[index] = b;
 
-	return (memory);
+	return (s);
 }
