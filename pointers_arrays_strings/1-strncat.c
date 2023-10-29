d5a541ecf59138802012ea6b97793144c3a723a6
aelitaa <gunel.sultanova@outlook.com>
2023-10-29 03:40:04 -0700

    n

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * _strncat - Concatenates the strings.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
- *
+ * @n: needed length.
  * Return: A pointer to the destination string @dest.
  */
 char *_strncat(char *dest, char *src, int n)
