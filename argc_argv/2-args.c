9ddcdfc810e8d6d75aa4235084034f671cbb5d0a
aelitaa <gunel.sultanova@outlook.com>
2023-11-05 01:48:47 -0800

    2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -6,11 +6,9 @@
  * Return: 0 - success.
  */
 int main(int argc, char *argv[]) {
-    int i;
+int i;
 
-    for (i = 1; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
-
-    return 0;
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
 }
