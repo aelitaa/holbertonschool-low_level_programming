7aa1b4a9a44b336c5e025f1ccdb10769907eca48
aelitaa <gunel.sultanova@outlook.com>
2023-11-19 04:25:55 -0800

    bunk

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,46 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings.
- * @n: The number of bytes to be allocated from s2.
+ * string_nconcat - Concatenates two strings using at
+ *                  most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
- *
- * Return: A pointer to the allocated memory.
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the concatenated space in memory.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int index_s2;
-int index_s1;
-int len_s1;
-int len_s2;
+	char *concat;
+	unsigned int len = n, index;
 
-if (s1 == NULL || s2 == NULL)
-{
-if (s1 == NULL)
-{
-s1 = " ";
-}else if (s2 == NULL)
-{
-s2 = " ";
-}
-}
-for (index_s2 = 0; s2(index_s2); index_s2++)
-{
-len_s2++;
-}
-if (n >= len_s2)
-{
-n = len_s2;
-}
-for (index_s1 = 0; s1[index_s1]; index_s1++)
-len_s1++;
-m = malloc(sizeof(char)*(len_s1+n+1));
-if (m == NULL)
-return (NULL);
-for (index_s2 = 0; index_s2 <= n; index_s2++)
-{
-m[len_s1] = m[index_s2]
-}
-return (m);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
