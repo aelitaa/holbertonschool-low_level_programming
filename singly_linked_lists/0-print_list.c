64fad3f451f24d466a20afa9416870f5e852169b
aelitaa <gunel.sultanova@outlook.com>
2023-12-05 03:41:33 -0800

    list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ int count = 0;
 	while (h != NULL)
 	{
 		if (h -> str == NULL)
-		printf ("[%d], %s", 0, "(nil)");
+		printf ("[%d] %s\n", 0, "(nil)");
 		else
-		printf ("[%d], %s", h->len, h->str);
+		printf ("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	count++;
 	}
