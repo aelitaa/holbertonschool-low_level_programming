47d082a1e5dd89061f559fc7da1416ed7474dea0
aelitaa <gunel.sultanova@outlook.com>
2023-12-05 03:45:13 -0800

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,10 +12,10 @@ size_t print_list(const list_t *h)
 int count = 0;
 	while (h != NULL)
 	{
-		if (h -> str == NULL)
-		printf ("[%d] %s\n", 0, "(nil)");
-		else
-		printf ("[%d] %s\n", h->len, h->str);
+	if (h->str == NULL)
+	printf("[%d] %s\n", 0, "(nil)");
+	else
+	printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	count++;
 	}
